e9da10ae0ac4b848245d97111edbb9f688e7c1bb
Nilamroychoudhury1999 <63612266+nilamroychoudhury1999@users.noreply.github.com>
2024-06-06 02:45:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,72 @@
 import React from 'react';
-import { Grid, Typography } from '@mui/material';
-import { StyledPaper, GridItem, Container } from './styles';
+import { Grid, Paper, Typography } from '@mui/material';
 
 const BaseLayout = () => {
     return (
         <>
             <Typography variant="h5" style={{ paddingLeft: '20px' }}>Performance Summary</Typography>
-            <Container container spacing={2} justifyContent="center">
-                <GridItem item xs={12} sm={6} md={6}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Performance Summary Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-                <GridItem item xs={12} sm={6} md={6}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Performance Summary Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-            </Container>
-            <Container container spacing={2} justifyContent="center">
-                <GridItem item xs={12} sm={6} md={4}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Performance Summary Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-                <GridItem item xs={12} sm={6} md={4}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Performance Summary Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-                <GridItem item xs={12} sm={6} md={4}>
-                    <StyledPaper elevation={3}>
+            <Grid container spacing={2} justifyContent="center" style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px', paddingBottom: '20px' }}>
+                <Grid item xs={12} sm={6} md={6} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
                         <Typography variant="h6">Performance Summary Card</Typography>
-                    </StyledPaper>
-                </GridItem>
-            </Container>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Performance Summary Card</Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
+            <Grid container spacing={2} justifyContent="center" style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px', paddingBottom: '20px' }}>
+                <Grid item xs={12} sm={6} md={4} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Performance Summary Card</Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Performance Summary Card</Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Performance Summary Card</Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
             <Typography variant="h5" style={{ paddingLeft: '20px' }}>Deployment Summary</Typography>
-            <Container container spacing={2} justifyContent="center">
-                <GridItem item xs={12} sm={6} md={4}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Deployment Summary Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-                <GridItem item xs={12} sm={6} md={4}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Deployment Summary Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-                <GridItem item xs={12} sm={6} md={4}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Deployment Summary Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-            </Container>
+            <Grid container spacing={2} justifyContent="center" style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px', paddingBottom: '20px' }}>
+                <Grid item xs={12} sm={6} md={4} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Deployment Summary Card</Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Deployment Summary Card</Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Deployment Summary Card</Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
             <Typography variant="h5" style={{ paddingLeft: '20px' }}>Opportunity Summary</Typography>
-            <Container container spacing={2} justifyContent="center">
-                <GridItem item xs={12} sm={6} md={6}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Opportunity Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-                <GridItem item xs={12} sm={6} md={6}>
-                    <StyledPaper elevation={3}>
-                        <Typography variant="h6">Opportunity Card </Typography>
-                    </StyledPaper>
-                </GridItem>
-            </Container>
+            <Grid container spacing={2} justifyContent="center" style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px', paddingBottom: '20px' }}>
+                <Grid item xs={12} sm={6} md={6} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Opportunity Card</Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Paper elevation={3} style={{ padding: '20px', textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '300px' }}>
+                        <Typography variant="h6">Opportunity Card</Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
         </>
     );
 };
 
 export default BaseLayout;
-
